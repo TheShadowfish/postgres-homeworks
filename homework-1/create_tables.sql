21d416cf57071c7fc682a2475aea6d618d189439
Эдуард Кусков <shadowfish@yandex.ru>
2024-03-26 21:30:49 +0300

    Сделано домашнее задание 17.1 Введение в работу с базами данных. Всё работает. В наличии таблиц в PgAdmin4 убедился лично.

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -3,14 +3,14 @@ CREATE DATABASE north;
 
 CREATE TABLE customers_data
 (
-    customer_id varchar(5) PRIMARY KEY,
+    customer_id varchar(5) UNIQUE PRIMARY KEY,
     company_name varchar(50) NOT NULL,
     contact_name varchar(50) NOT NULL
 );
 
 CREATE TABLE employees_data
 (
-    employee_id int PRIMARY KEY,
+    employee_id int UNIQUE PRIMARY KEY,
 	first_name varchar(50) NOT NULL,
     last_name varchar(50) NOT NULL,
 	title varchar(50) NOT NULL,
@@ -20,9 +20,9 @@ CREATE TABLE employees_data
 
 CREATE TABLE orders_data
 (
-	order_id int PRIMARY KEY,
-	customer_id varchar(5) UNIQUE REFERENCES employees_data(employee_id),
-	employee_id int UNIQUE REFERENCES employees_data(employee_id),
+	order_id int UNIQUE PRIMARY KEY,
+	customer_id varchar(5) REFERENCES customers_data(customer_id),
+	employee_id int REFERENCES employees_data(employee_id),
 	order_date date NOT NULL,
 	ship_city varchar(50) NOT NULL
 );
--- homework-1/main.py
+++ homework-1/main.py
@@ -4,26 +4,28 @@ import os
 import csv
 
 
-
-def script_in_main_py():
+def script_in_main_py(write_bd: bool):
     """
     Написать скрипт в main.py, который заполнит созданные таблицы данными из north_data
-    Для подключения к БД использовать библиотеку psycopg2
-    Зайти в pgAdmin и убедиться, что данные в таблицах есть
+    Для подключения к БД использовать библиотеку psycopg2.
+    Зайти в pgAdmin и убедиться, что данные в таблицах есть.
     """
-    # customers_data
-    #data_list = read_from_file("customers_data.csv")
-    #print(data_list)
+    if write_bd:
+        # customers_data
+        data_list = read_from_file("customers_data.csv")
+        write_to_database("customers_data", data_list)
+        # employees_data
+        data_list = read_from_file("employees_data.csv")
+        write_to_database("employees_data", data_list)
+        # orders_data
+        data_list = read_from_file("orders_data.csv")
+        write_to_database("orders_data", data_list)
+
     print_database_table("customers_data")
-    # write_to_database("customers_data", data_list)
-
-    # employees_data
-    data_list = read_from_file("employees_data.csv")
-    print(data_list)
     print_database_table("employees_data")
-    write_to_database("employees_data", data_list)
+    print_database_table("orders_data")
 
-    pass
+
 
 def read_from_file(filename: str = '') -> dict:
     """
@@ -35,11 +37,11 @@ def read_from_file(filename: str = '') -> dict:
     if not os.path.exists(filepath):
         raise FileNotFoundError(f"file {filepath} not found!")
         data_csv = []
-
     with open(filepath) as my_csv:
         reader = csv.DictReader(my_csv)
         data_csv = list(reader)
     return data_csv
+
 
 def print_database_table(tablename):
     conn_params = {
@@ -48,7 +50,7 @@ def print_database_table(tablename):
         "user": "postgres",
         "password": "12345"
     }
-    print(f"Таблица в базе north(localhost) {tablename}: \n")
+    print(f"\n Таблица в базе north(localhost) {tablename}: \n")
 
     with psycopg2.connect(**conn_params) as conn:
         with conn.cursor() as cur:
@@ -67,39 +69,17 @@ def write_to_database(tablename, cvs_data: list[dict]):
         "user": "postgres",
         "password": "12345"
     }
-    # dict_0 = cvs_data[0]
-    # print(dict_0)
-    # print(dict_0.keys())
-    num_of_par = len(cvs_data[0])
-    print(f"num_of_par = {num_of_par}")
+    # количество параметров VALUES в INSERT INTO, т.е. (%s, %s, ... %s)
+    string_s = ', '.join(['%s' for i in range(len(cvs_data[0]))])
 
-    string_s = ', '.join(['%s' for i in range(num_of_par)])
-    print(f"%s: ({string_s})")
-
-    tuple_par = tuple(cvs_data[0].values())
-    print(f"vars: {tuple_par}")
-
-    if (tablename == 'customers_data'):
-        tuple_string = [tuple(line.values()) for line in cvs_data]
-    elif tablename == 'employees_data':
-        '''    !!! employee_id int PRIMARY KEY,
-	first_name varchar(50) NOT NULL,
-    last_name varchar(50) NOT NULL,
-	title varchar(50) NOT NULL,
-	!!! birth_date date NOT NULL,
-	? notes text NOT NULL'''
-
-
-        return
-
-
-    # return
-
+    # превращает числовые значения в числа, строковые не меняет
+    int_from_str = lambda x: int(x) if x.isdigit() else x
+    # список кортежей для cur.executemany
+    tuple_string = [tuple([int_from_str(v) for v in line.values()]) for line in cvs_data]
 
     conn2 = psycopg2.connect(**conn_params)
     cur = conn2.cursor()
-    print(f"INSERT INTO {tablename} VALUES ({string_s}) {tuple_string}")
-
+    # print(f"INSERT INTO {tablename} VALUES ({string_s}) {tuple_string}")
 
     try:
         cur.executemany(f"INSERT INTO {tablename} VALUES ({string_s})", tuple_string)
@@ -113,52 +93,7 @@ def write_to_database(tablename, cvs_data: list[dict]):
         conn2.close()
 
 
-
 # начало программы
 if __name__ == '__main__':
-    script_in_main_py()
+    script_in_main_py(True)
 
-
-
-"""
-    orders_data.csv
-
-order_id
-customer_id
-employee_id
-order_date
-ship_city
-
-10248
-VINET
-5
-1996-07-04
-Reims
-
-
-    employees_data.csv
-
-employee_id
-first_name
-last_name
-title
-birth_date
-notes
-
-1
-Nancy
-Davolio
-Sales Representative
-1948-12-08
-Education includes a BA in psychology from Colorado State University in 1970.  She also completed The Art of the Cold Call.  Nancy is a member of Toastmasters International.
-
-    customers_data.csv
-
-customer_id
-company_name
-contact_name
-
-ALFKI
-Alfreds Futterkiste
-Maria Anders
-"""
