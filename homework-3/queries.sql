a95587b2765228a83be0a3e6820e9b8b6641d8ba
TheShadowfish <shadowfish@yandex.ru>
2024-04-02 13:37:06 +0300

    HW 3-1 +, варианты с join-using, join-on

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,15 +1,27 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
+
+--используем WHERE, AND--
 SELECT customers.company_name, CONCAT(first_name, ' ', last_name) FROM customers, employees, orders, shippers
 	WHERE customers.customer_id = orders.customer_id AND  employees.employee_id = orders.employee_id
 	AND shippers.shipper_id = ship_via AND shippers.company_name = 'United Package'
+	AND customers.city = 'London' AND employees.city = 'London';
+
+-- используем JOIN, USING, ON--
+SELECT customers.company_name, CONCAT(first_name, ' ', last_name) FROM orders
+	JOIN customers USING (customer_id)
+	JOIN employees USING (employee_id)
+	JOIN shippers ON shippers.shipper_id = orders.ship_via
+	WHERE shippers.company_name = 'United Package'
 	AND customers.city = 'London' AND employees.city = 'London';
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
+
+--используем WHERE, AND--
 SELECT products.product_name, products.units_in_stock, suppliers.contact_name, suppliers.phone FROM products, suppliers, categories
 	WHERE products.supplier_id = suppliers.supplier_id
 	AND  products.discontinued <> 1 AND products.units_in_stock < 25
@@ -17,11 +29,26 @@ SELECT products.product_name, products.units_in_stock, suppliers.contact_name, s
 	AND category_name IN ('Dairy Products','Condiments')
 	ORDER BY products.units_in_stock;
 
+-- используем JOIN, USING, ON--
+SELECT products.product_name, products.units_in_stock, suppliers.contact_name, suppliers.phone FROM products
+	JOIN suppliers USING (supplier_id)
+	JOIN categories USING (category_id)
+	WHERE products.discontinued <> 1 AND products.units_in_stock < 25
+	AND category_name IN ('Dairy Products','Condiments')
+	ORDER BY products.units_in_stock;
+
+
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
+--через EXPECT--
 SELECT customers.company_name FROM customers
 EXCEPT
 SELECT customers.company_name FROM customers, orders
 WHERE customers.customer_id = orders.customer_id;
+
+--с подзапрпосом--
+SELECT customers.company_name FROM customers
+WHERE customers.company_name NOT IN (SELECT customers.company_name FROM customers, orders
+WHERE customers.customer_id = orders.customer_id);
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
@@ -49,3 +76,9 @@ SELECT DISTINCT product_name FROM products,
 SELECT DISTINCT product_name FROM orders,
 (SELECT * FROM products, order_details WHERE order_details.product_id = products.product_id AND order_details.quantity = 10) AS order_products_10
 WHERE orders.order_id = order_products_10.order_id;
+
+
+-- Шесть!!! ---
+SELECT DISTINCT product_name FROM (SELECT * FROM order_details WHERE quantity = 10) AS details
+	JOIN products USING(product_id)
+	JOIN orders USING(order_id);
